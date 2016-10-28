2dc7e6c270fd9699e7b42871e9e5381663c2b02e
Erkan Sen <erkansen14@hotmail.com>
2016-10-28 16:22:44 -0500

    react tabbar

--- client/Components/App.js
+++ client/Components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-
+import { Tabs, Tab } from 'react-bootstrap'
 export default class App extends Component {
 
   render() {
     return(
-      <div>
-        YOLO SWAG
-      </div>
+      <Tabs defaultActiveKey={2} id="uncontrolled-tab-example" bsStyle='pills'>
+        <Tab eventKey={1} title="Tab 1" />
+        <Tab eventKey={2} title="Tab 2" />
+        <Tab eventKey={3} title="Tab 3" />
+      </Tabs>
     )
   }
 }
